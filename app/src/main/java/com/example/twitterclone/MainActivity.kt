d7ec00e42d7b8bad53d5cf411ed0641884360479
Ann-Okoyo <annmuyale@gmail.com>
2023-08-27 15:07:34 +0300

    second

--- app/src/main/java/com/example/twitterclone/MainActivity.kt
+++ app/src/main/java/com/example/twitterclone/MainActivity.kt
@@ -2,26 +2,38 @@ package com.example.twitterclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.inputmethod.InputBinding
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.twitterclone.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBind
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        displayTweets()
+    }
+
+    private fun displayTweets() {
+        val tweet1 = TweetData("Pau", "Ann", "@popo", "i love memes", 12, 23, 4)
+        val tweet2 = TweetData("Nora", "Charles", "@nia", "i love travelling all over Kenya", 12, 23, 212)
+        val tweet3 = TweetData("Mike", "Michael", "@mmike", "coding might be hard but it's really fun", 12, 23, 42)
+        val tweet4 = TweetData("Sam", "Samael", "@saml", "The largest organ in the body is the skin", 12, 23, 482)
+        val tweet5 = TweetData("Pau", "Ann", "@popo", "i love memes", 12, 23, 4)
+        val tweet6 = TweetData("Nora", "Norani", "@nia", "i love travelling all over Kenya", 12, 23, 212)
+        val tweet7 = TweetData("Mike", "Michael", "@mmike", "coding might be hard but it's really fun", 12, 23, 42)
+        val tweet8 = TweetData("Sam", "Samael", "@saml", "The largest organ in the body is the skin", 12, 23, 482)
+        val tweet9 = TweetData("Pau", "Ann", "@popo", "i love memes", 12, 23, 4)
+        val tweet10 = TweetData("Nora", "Norani", "@nia", "i love travelling all over Kenya", 12, 23, 212)
+        val tweet11 = TweetData("Mike", "Michael", "@mmike", "coding might be hard but it's really fun", 12, 23, 42)
+        val tweet12 = TweetData("Sam", "Samael", "@saml", "The largest organ in the body is the skin", 12, 23, 482)
+
+        val tweetLists = listOf(tweet1, tweet2, tweet3, tweet4, tweet5, tweet6, tweet7, tweet8, tweet9, tweet10, tweet11, tweet12)
+
+        binding.rvTweets.layoutManager = LinearLayoutManager(this)
+        val adapterTweets = TwitterRVAdapter(tweetLists)
+        binding.rvTweets.adapter = adapterTweets
     }
 }
-fun displayTweets(){
-    var tweet1 =TweetData("Hello world","Ann Muyale","Wakahia","Mobile developer",71,405,890)
-    var tweet2 =TweetData("Hello world","Ann Muyale","Wakahia","Mobile developer",71,405,890)
-    var tweet3 =TweetData("Hello world","Ann Muyale","Wakahia","Mobile developer",71,405,890)
-    var tweet4=TweetData("Hello world","Ann Muyale","Wakahia","Mobile developer",71,405,890)
-    var tweetList= listOf<TweetData>(tweet1,tweet2,tweet3,tweet4)
-    binding.rvTweets.layoutManager=LinearLayoutManager
-    var tweetsRvAdapter=tweetsrvAdapter(tweetList)
-    binding.rvTweets
-
-
-}
-
--- app/src/main/java/com/example/twitterclone/TweetsRvAdapter.kt
+++ app/src/main/java/com/example/twitterclone/TweetsRvAdapter.kt
@@ -1,24 +1,34 @@
 package com.example.twitterclone
 
-
 import android.view.LayoutInflater
-import android.view.SurfaceHolder
 import android.view.ViewGroup
-import android.view.ViewParent
 import androidx.recyclerview.widget.RecyclerView
+import com.example.twitterclone.databinding.TweetListItemBinding
 
-class TweetsRvAdapter (var tweetsList: List<TweetData>):RecyclerView.Adapter {
+class TwitterRVAdapter(var tweetsList: List<TweetData>): RecyclerView.Adapter<TweetsViewHolder>() {
 
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TweetViewHolder {
-        var binding = TweetListItemBinding.inflate(LayoutInflater.from(parent))
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TweetsViewHolder {
+        var binding = TweetListItemBinding.inflate(LayoutInflater.from(parent.context),
+            parent, false)
         return TweetsViewHolder(binding)
-
     }
-    override  fun onBindViewHolder(holder: TweetsViewHolder,position:Int)
-      var tweet=tweetsList[position]
-      holder.binding.tvDispla
+
+    override fun onBindViewHolder(holder: TweetsViewHolder, position: Int) {
+        var tweetData = tweetsList[position]
+        holder.binding.apply {
+            tvDisplayName.text = tweetData.displayName
+            tvHandle.text = tweetData.handle
+            tvTweet.text = tweetData.tweet
+            tvReplyCount.text = tweetData.replyCount.toString()
+            tvRtCount.text = tweetData.rtCount.toString()
+            tvLikeCount.text = tweetData.likeCount.toString()
+
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return tweetsList.size
+    }
 }
 
-
-
+class TweetsViewHolder(var binding: TweetListItemBinding): RecyclerView.ViewHolder(binding.root)
